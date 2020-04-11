afea9683d4e8c6f4f35d315924bdbb980fa9b560
jochen@homeland <no-go@localhost>
2020-04-11 21:26:45 +0200

    fade in and fade out if it is not really dark or bright. this hack stops stupid blink in twielight situations.

--- trinket_kerze/trinket_kerze.ino
+++ trinket_kerze/trinket_kerze.ino
@@ -11,16 +11,37 @@ void setup() {
 }
 
 void loop() {
+  // is it dark ? ----------------------
   if (analogRead(A1) > 200) {
+    
+    // fade on !! ----------------------
+    if (alloff == true) {
+      for (int j=0; j<255; ++j) {
+        for (int i=0; i<3; ++i) analogWrite(pins[i], j); 
+        delay(40+(64-j/4));
+      }
+    }
+    
     alloff = false;
+    // flame effect! -------------------
     flame[2] = flame[1];
     flame[1] = flame[0];
     flame[0] = random(80, 254);
     delay(random(30, 150));
-    for (int i=0; i<3; ++i) analogWrite(pins[i], flame[i]);    
+    for (int i=0; i<3; ++i) analogWrite(pins[i], flame[i]);
+      
   } else {
+    
     delay(200);
     if (alloff == false) {
+      
+      // fade off !! ------------------
+      for (int j=0; j<255; ++j) {
+        for (int i=0; i<3; ++i) analogWrite(pins[i], 255-j); 
+        delay(40+(j/4));
+      }
+
+      // off -------------------------
       for (int i=0; i<3; ++i) digitalWrite(pins[i], LOW);
       alloff = true;
     }
